feda6e0c6084e16bd551db1d32e038f3136e3234
HMFifth <WZZ@wangzhizhongMacBook-Pro.local>
2015-11-19 23:55:21 +0800

    tabBar

---
+++ CustomTabBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomTabBar
+//
+//  Created by zhangshuai on 15/11/4.
+//  Copyright © 2015年 zhangshuai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
